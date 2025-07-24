c8cff4c9b6802cc82aadb3dc7c27195402eeb680
Dawid Lesniak <dawid215@outlook.com>
2025-07-24 20:45:11 +0200

    Enhance To Do App: Add task creation functionality with validation and styling updates

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const addTaskButton = document.getElementById('addTask');
 const container = document.getElementById('container');
 
 function addTask() {
+    //dodawanie zadania
     const div = document.createElement('div');
     div.classList.add('taskCreatorDiv', 'taskCreatorDivIn');
     const blur = document.createElement('div');
@@ -12,37 +13,129 @@ function addTask() {
 
     const cancelButton = document.createElement('button');
     cancelButton.textContent = 'X';
+    cancelButton.classList.add('cancelButton');
 
     const titleInput = document.createElement('input');
     titleInput.type = 'text';
     titleInput.placeholder = 'Tytuł zadania';
     titleInput.classList.add('titleInput');
 
-    const descriptionInput = document.createElement('input');
+    const descriptionInput = document.createElement('textarea');
     descriptionInput.type = 'text';
     descriptionInput.placeholder = 'Opis zadania';
-    titleInput.classList.add('descriptionInput');
+    descriptionInput.classList.add('descriptionInput');
 
     const dateInput = document.createElement('input');
     dateInput.type = 'date';
     dateInput.min = new Date().toISOString().split('T')[0];
+    dateInput.classList.add('dateInput');
 
     const timeInput = document.createElement('input');
     timeInput.type = 'time';
     timeInput.disabled = true;
+    timeInput.classList.add('timeInput');
 
-    if(dateInput.value !== '')
-    {
-        timeInput.disabled = false;
-        timeInput.defaultValue = '08:00';
-    }
+    const priorityInput = document.createElement('select');
+    const priotities = ['Niski', 'Średni', 'Wysoki'];
+    priorityInput.classList.add('priorityInput');
+    priotities.forEach(priority => {
+        const option = document.createElement('option');
+        option.value = priority;
+        option.textContent = priority;
+        option.defaultSelected = priority === 'Niski'; // domyślnie niski priorytet
+        priorityInput.appendChild(option);
+    });
 
-    div.appendChild(cancelButton);
+    const inputContainer = document.createElement('div');
+    inputContainer.style.display = 'flex';
+    inputContainer.style.justifyContent = 'left';
+    inputContainer.style.width = '90%';
+    //inputContainer.style.marginLeft = '4.5%';
+    //inputContainer.style.marginTop = '1%';
+    inputContainer.style.margin = '1% 0 5px 4.5%';
+    inputContainer.style.gap = '10px';
+
+    dateInput.addEventListener('change', () => {
+        if(dateInput.value !== '')
+        {
+            timeInput.disabled = false;
+            timeInput.defaultValue = '08:00'; //po wybraniu daty ustwa domyślną godzinę
+        }
+    })
+
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Dodaj zadanie';
+    submitButton.disabled = true;
+    submitButton.classList.add('submitButton');
+
     div.appendChild(titleInput);
+    div.appendChild(cancelButton);
     div.appendChild(descriptionInput);
-    div.appendChild(dateInput);
-    div.appendChild(timeInput);
+    div.appendChild(inputContainer);
+    inputContainer.appendChild(dateInput);
+    inputContainer.appendChild(timeInput);    
+    inputContainer.appendChild(priorityInput);
+    div.appendChild(submitButton);
 
+
+    //sprawdzanie czy inputy nie są puste
+    validateForm = () => {
+        if (titleInput.value && descriptionInput.value && dateInput.value && timeInput.value)
+        {
+            submitButton.disabled = false;
+        }
+        else
+        {
+            submitButton.disabled = true;
+        }
+    }
+    titleInput.addEventListener('change',validateForm);
+    descriptionInput.addEventListener('change',validateForm);
+    dateInput.addEventListener('change',validateForm);
+    timeInput.addEventListener('change',validateForm);
+
+
+    //finalizowanie dodawania zadania
+    submitButton.addEventListener('click', () => {
+        //animacja dodawania i usuwanie okna
+        div.classList.add('taskCreationDivAdded');
+        blur.classList.add('blurOutAnimation')
+        console.log('added')
+        setTimeout(() => {
+            div.remove();
+            blur.remove();
+        }, 800);
+
+        //dodawanie taska do listy
+        const tasklist = document.querySelector('.taskListContent ul');
+        //console.log(tasklist);
+        const li = document.createElement('li');
+        li.style.visibility = 'hidden';
+        const h2 = document.createElement('h2');
+        h2.textContent = titleInput.value;
+
+        const description = document.createElement('p');
+        description.textContent = descriptionInput.value;
+
+        const date = document.createElement('p');
+        date.classList.add('dateTime');
+        date.textContent = `${dateInput.value} ${timeInput.value}`;
+
+        
+        tasklist.prepend(li);
+        li.appendChild(h2);
+        li.appendChild(description);
+        li.appendChild(date);
+        setTimeout(() => {
+            li.style.visibility = 'visible';
+            li.classList.add('slide-In');
+        },800);
+        setTimeout(() => {
+            li.classList.remove('slide-In');
+        }, 3000);
+    })
+
+    //zamykanie okna
     cancelButton.addEventListener('click', () => {
         blur.classList.remove('blurInAnimation');
         blur.classList.add('blurOutAnimation')
